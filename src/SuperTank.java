bd6a24f48ad952ca1a8465628b67f7a3f91a7e66
Kaif Ahsan <kahsan@student.unimelb.edu.au>
2020-06-11 13:06:39 +1000

    Fixed supertank shooting bug

--- src/SuperTank.java
+++ src/SuperTank.java
@@ -4,9 +4,11 @@ public class SuperTank extends Tower {
     private static final int COST = 600;
     private static final String IMAGE = "res/images/supertank.png";
     private static final int RADIUS_ST = 150;
+    private static final double SUPER_COOLDOWN = 0.5;
 
     public SuperTank(Point point) {
         super(point, IMAGE, COST);
         this.radius = RADIUS_ST;
+        this.cooldown = SUPER_COOLDOWN;
     }
 }
